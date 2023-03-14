53c2b008889c56093998f566d898e93bc17f1f85
Farhaz02 <ffarhaz800@gmail.com>
2023-03-14 11:15:12 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+$('.banner-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+   
+  });
+  
+
+  $('.main-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   
+  });
+  
+
+  
